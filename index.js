14f276a096579178fb5098b41052af3f5b4e05e8
sykes836 <bernard.sykes836@gmail.com>
2018-08-16 18:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var driver = {};
 function updateDriverWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+function destructivelyUpdateDriverWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
-function deleteFromObjectByKey(object, key){
+function deleteFromDriverByKey(object, key){
   return Object.assign({}, object, key)
 }
-function destructivelyDeleteFromObjectByKey(object, key){
+function destructivelyDeleteFromDriverByKey(object, key){
   delete object.key
   return object
 } 
